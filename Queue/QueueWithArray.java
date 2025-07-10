ec6aab54075ecfa646998c5addde599dfb43d3c6
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-07-10 16:17:05 +0530

    queue

--- Queue/QueueWithArray.java
+++ Queue/QueueWithArray.java
@@ -15,9 +15,28 @@ public class QueueWithArray{
         return toq==queue.length-1 || boq ==queue.length;
     }
 
+    public void enQueue(int data){
+        if(isFull()){
+            System.out.println("Queue is full");
+            return;
+        }else if(isEmpty()){
+            boq = 0;
+            toq = 0;
+            queue[ ++toq] = data;
+            System.out.println("Enqueued: " + data);
+        }else{
+            
+            queue[++toq] = data;
+             System.out.println("Enqueued: " + data);
+        }
+    }
+
     public static void main(String[] args) {
         QueueWithArray queue = new QueueWithArray(5);
-        
+
         System.out.println("Is queue empty? " + queue.isEmpty());
+        queue.enQueue(10);
+        queue.enQueue(20);
+        queue.enQueue(30);
     }
 }
